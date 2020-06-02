42a3fa220cc6ac6adafc8e9fdc0b82462c28b316
Gyanendra Verma <gveeiit@gmail.com>
2020-06-03 05:08:42 +0530

    Update Ds18b20-parasite power mode.ino

--- Ds18b20-parasite power mode.ino
+++ Ds18b20-parasite power mode.ino
@@ -1,3 +1,7 @@
+/*   Copyright [2020] [Gyanendra Verma]
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.*/
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
